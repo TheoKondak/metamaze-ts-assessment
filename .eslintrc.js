9377a823ed89f5f7f8c9c5a7a483d4e9f761ba7d
theokondak <thkondak@gmail.com>
2024-01-27 14:16:10 +0100

    chore: environment configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,4 +8,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
 };
